82593fda821c350415d27f928ec4c537a5f58599
Soumyabrata-edu <115734398+Soumyabrata-edu@users.noreply.github.com>
2023-01-06 13:26:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://Soumyabrata-edu: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@github.com/Soumyabrata-edu/Marie && cd Marie && pip3 install -U -r requirements.txt && nohup python3 -m tg_bot &")
+os.system("git clone https://Soumyabrata-edu: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@github.com/Soumyabrata-edu/erisbot && cd erisbot && pip3 install -U -r requirements.txt && nohup python3 -m eris &")
